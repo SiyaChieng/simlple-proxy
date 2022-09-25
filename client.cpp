df0c84f590bc26cc3d8ed85cd4463b2a99bd5914
SiyaChieng <yaochichen2016@gmail.com>
2022-09-26 07:24:06 +0800

    update

---
+++ client.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <iostream>
+// #include <boost/asio.hpp>
+
+int main(int argc, char** argv) {
+    if(argc < 3) {
+        std::cerr << "usage: " << argv[0] << " CONFIG_PATH IP:PORT\n";
+        exit(EXIT_FAILURE);
+    }
+
+    std::string str_config_path(argv[1]);
+    std::string str_ip_port(argv[2]);
+
+    auto loc = str_ip_port.find(':');
+    if(loc == std::string::npos) {
+        std::cerr << "IP:PORT invalid\n";
+        exit(EXIT_FAILURE);
+    }
+    std::string str_ip = str_ip_port.substr(0, loc);
+    std::string str_port = str_ip_port.substr(loc+1);
+
+    std::cout << "config_path[" << str_config_path << "] ip[" 
+        << str_ip << "] port[" << str_port << "]\n";
+
+    return 0;
+}
